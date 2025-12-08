3280b84303b970fc6dc9033adfec9f342e2ad686
ZAHID Abdelouahed <abdelouahed.zahid4@etu.univ-lorraine.fr>
2025-12-08 09:34:18 +0000

    Delete DeliveryRepository.java

--- livraison_backend/livraison_backend/src/main/java/com/example/livraison_backend/repo/DeliveryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.livraison_backend.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.livraison_backend.model.Delivery;
-
-public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
-}
