180bc164a6d74abcc8a13ed8b34bd64817c4cc45
ZAHID Abdelouahed <abdelouahed.zahid4@etu.univ-lorraine.fr>
2025-12-08 09:34:27 +0000

    Delete OrderRepository.java

--- livraison_backend/livraison_backend/src/main/java/com/example/livraison_backend/repo/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.livraison_backend.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.livraison_backend.model.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
