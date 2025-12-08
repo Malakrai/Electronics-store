e15a3abc314ecdad2fa5e89bf9ba55f0f466c8b6
ZAHID Abdelouahed <abdelouahed.zahid4@etu.univ-lorraine.fr>
2025-12-08 09:34:43 +0000

    Delete TrackingEventRepository.java

--- livraison_backend/livraison_backend/src/main/java/com/example/livraison_backend/repo/TrackingEventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.livraison_backend.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.livraison_backend.model.TrackingEvent;
-import java.util.List;
-
-public interface TrackingEventRepository extends JpaRepository<TrackingEvent, Long> {
-    List<TrackingEvent> findByDeliveryIdOrderByCreatedAtAsc(Long deliveryId);
-}
