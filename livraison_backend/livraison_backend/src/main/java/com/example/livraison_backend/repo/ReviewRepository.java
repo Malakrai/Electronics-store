79b1d40bc9082b26b43647eabd9dbd07fdcf057b
ZAHID Abdelouahed <abdelouahed.zahid4@etu.univ-lorraine.fr>
2025-12-08 09:34:35 +0000

    Delete ReviewRepository.java

--- livraison_backend/livraison_backend/src/main/java/com/example/livraison_backend/repo/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.livraison_backend.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.livraison_backend.model.Review;
-import java.util.List;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-    List<Review> findByOrderId(Long orderId);
-}
