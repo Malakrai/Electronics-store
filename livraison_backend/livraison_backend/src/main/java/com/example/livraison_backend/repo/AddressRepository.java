9d1becfe1f53755e630b6bb9ef7c9957ae5a8a1e
ZAHID Abdelouahed <abdelouahed.zahid4@etu.univ-lorraine.fr>
2025-12-08 09:34:10 +0000

    Delete AddressRepository.java

--- livraison_backend/livraison_backend/src/main/java/com/example/livraison_backend/repo/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.livraison_backend.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.livraison_backend.model.Address;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
